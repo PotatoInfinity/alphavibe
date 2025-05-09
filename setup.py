78a5b6f44ca2a57e39b88dea4aa8a275c3eeb706
Trương Minh Huy <developerlouis7923@gmail.com>
2025-05-10 05:49:53 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='genalpha-lang',
+    name='alphavibe',
     version='1.0.0',
     description='Gen Alpha programming language: Vibe-based Python for Zoomers',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Louis the Sigma',
-    author_email='sigma@slayverse.com',
+    author_email='developerlouis7923@gmail.com',
     url='https://github.com/PotatoInfinity/genalpha-lang',
     packages=find_packages(),
     classifiers=[
