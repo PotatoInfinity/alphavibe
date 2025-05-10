dc9bc5e3d7051ecfa9327460436e9b4ffd875c8d
Trương Minh Huy <developerlouis7923@gmail.com>
2025-05-10 07:04:53 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='alphavibe',
-    version='1.0.0',
+    version='1.5.0',
     description='Gen Alpha programming language: Vibe-based Python for Zoomers',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
